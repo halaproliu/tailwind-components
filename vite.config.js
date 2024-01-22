916053562ec05ff2f51c21b91b36981416711c87
luffy <luffybryant@gmail.com>
2024-01-22 22:25:22 +0800

    feat: 修改发布生产的base路径

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig, } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path'
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "docs",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_NODE_ENV === "production" ? "/tailwind-components" : "/",
+    plugins: [react()],
+    build: {
+      outDir: "docs",
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  };
 });
